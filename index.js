844e11073b94594fd234ce527d799139f5055bf5
cnnr-adams <cnnr252@gmail.com>
2019-03-10 13:28:19 -0400

    sent to frontend

--- index.js
+++ index.js
@@ -2,11 +2,34 @@ const port = process.env.PORT || 8080;
 
 const express = require("express");
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
+let imgUpvoteMap = new Map();
+imgUpvoteMap.set("a", { votes: 0, path: "/..", sortPosition: 0 });
+imgUpvoteMap.set("b", { votes: 0, path: "/..", sortPosition: 0 });
+imgUpvoteMap.set("c", { votes: 0, path: "/..", sortPosition: 0 });
+imgUpvoteMap.set("d", { votes: 0, path: "/..", sortPosition: 0 });
+imgUpvoteMap.set("e", { votes: 0, path: "/..", sortPosition: 0 });
+imgUpvoteMap.set("f", { votes: 0, path: "/..", sortPosition: 0 });
+imgUpvoteMap.set("g", { votes: 0, path: "/..", sortPosition: 0 });
+imgUpvoteMap.set("h", { votes: 0, path: "/..", sortPosition: 0 });
+imgUpvoteMap.set("i", { votes: 0, path: "/..", sortPosition: 0 });
+imgUpvoteMap.set("j", { votes: 0, path: "/..", sortPosition: 0 });
 // const favicon = require("serve-favicon");
 // const path = require('path');
 
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+io.on('connection', function (socket) {
+    console.log(imgUpvoteMap);
+    socket.emit('data', JSON.stringify(Array.from(imgUpvoteMap)));
+    socket.on('voteChange', (name, obj) => {
+        imgUpvoteMap.set(name, obj);
+        socket.broadcast.emit('data', imgUpvoteMap);
+    });
+});
+
 
 /* serves main page */
 app.get("/", function (req, res) {
@@ -14,6 +37,6 @@ app.get("/", function (req, res) {
 });
 
 app.use(express.static(__dirname + '/public'));
-app.listen(port, function () {
+http.listen(port, function () {
     console.log("Listening on " + port);
 });
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,1 +1,12 @@
-console.log("Template Script!");
+console.log("test");
+var socket = io();
+socket.emit('test', "hi");
+socket.on('data', (data) => {
+    data = new Map(JSON.parse(data));
+    data.forEach((value, name) => {
+        var el = document.getElementById(name);
+        console.log(el);
+        el.childNodes.getElementsByTagName("img")[0].src = value.path;
+        el.childNodes.getElementsByClassName("vote roundrect")[0].getElementsByClassName("count")[0].body = value.votes;
+    });
+})
