03ebb2d8d36fb7a23232f9235908bf3aa6028231
cnnr-adams <cnnr252@gmail.com>
2019-03-10 12:28:19 -0400

    socket.io stuff

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const port = process.env.PORT || 8080;
 
 const express = require("express");
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+let imgUpvoteMap = new Map();
+imgUpvoteMap.set("a", { votes: 0, path: "/..", sortPosition: 0 });
 
 // const favicon = require("serve-favicon");
 // const path = require('path');
@@ -9,11 +13,22 @@ const app = express();
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 /* serves main page */
+
+io.on('connection', function (socket) {
+    socketMap.set()
+    socket.emit('data', imgUpvoteMap);
+    socket.on('voteChange', (name, obj) => {
+        imgUpvoteMap.set(name, obj);
+        socket.broadcast.emit('data', imgUpvoteMap);
+    });
+});
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.use(express.static(__dirname + '/public'));
-app.listen(port, function () {
+http.listen(port, function () {
     console.log("Listening on " + port);
 });
+
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,1 +1,6 @@
-console.log("Template Script!");
+console.log("test");
+var socket = io();
+socket.emit('test', "hi");
+socket.on('data', (data) => {
+
+})
